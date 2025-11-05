eeb19968659ad3865cfc14d4fc1d91f6697268a0
LuthGom <lucianomendes23@yahoo.com>
2025-11-05 20:16:13 -0300

    refactor(page): importing apii

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
-import CardsList from "./components/CardsList";
+import CardsList from "./components/lists/CardsList";
+import { api } from './services/api';
 
-
-
-export default async function Home({searchParams}: {searchParams?: {page?: string}}) {
-
-  const currentPage = Number(searchParams?.page || 1);
-  
-
-  const URL ="https://pluga.co/ferramentas_search.json";
-  // const tools = await data.json();
-
-  
+export default async function Home({ searchParams }: { searchParams?: { page?: string } }) {
 
   return (
-    
     <>
       <main className="w-4/5 mx-auto ">
 
-        <div className="w-4/5 mx-auto p-4 m-3 border-2 border-black-500 border-solid">
-
-          <input className="w-full p-4" type="search" name="search_tool" id="search_tool" placeholder="BUSCAR FERRAMENTA" />
-        </div>
-
-        <CardsList  URL={URL}  ></CardsList>
+        <CardsList URL={api}  ></CardsList>
       </main>
     </>
   );
